1d7df57255f4d253cc261b4f2c619db4a8c12db3
Nasker <xamps23@gmail.com>
2020-09-01 18:03:29 +0200

    Added multiplatform serial support, so to speak

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import platform
 import sys
 from PyQt5 import QtWidgets as QtW
 from PyQt5 import QtCore as QtC
@@ -5,13 +6,27 @@ from PyQt5 import QtGui as QtG
 from cvi_gui import Ui_Form
 from CVIMonochromator import CVIMonochromator
 
+serial_device = ''
+
+if platform.system() == 'Darwin':
+    print("I'm on macOS!")
+    serial_device = '/dev/tty.usbserial'
+elif platform.system() == 'Windows':
+    print("I'm on Windows!")
+    serial_device = 'COM3'
+elif platform.system() == 'Linux':
+    print("I'm on Linux!")
+    serial_device = '/dev/ttyUSB0'
+else:
+    print("Fuck, don't know the platform I'm running...")
+
 
 class MainWindow(QtW.QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.ui = Ui_Form()
         self.ui.setupUi(self)
-        self.cvi_monochromator = CVIMonochromator("COM5")
+        self.cvi_monochromator = CVIMonochromator(serial_device)
 
         self.ui.unitsLabel.setText("nm")
         self.ui.gotoButton.clicked.connect(self.goto_function)
